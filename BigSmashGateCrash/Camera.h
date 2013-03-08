7e161292b207524ef2794bf78bd4b34e08e0d194
Mårten Möller <jonkel.lino@gmail.com>
2013-03-08 14:06:53 +0100

    warning removal

--- BigSmashGateCrash/Camera.h
+++ BigSmashGateCrash/Camera.h
@@ -9,7 +9,7 @@ public:
 	~Camera(void);
 
 public:
-	const Vector2f const GetPosition() const { return myPosition; }
+	const Vector2f GetPosition() const { return myPosition; }
 	void SetPosition( const Vector2f& aPosition ){ myPosition = aPosition; }
 	void Move( const Vector2f& aMovement ){ myPosition += aMovement; }
 
